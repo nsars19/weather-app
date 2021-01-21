bbdc9668b05d70702398c4b70648bc2bd1b23f3d
Nick <nsardonini@gmail.com>
2021-01-21 13:15:01 -0500

    add update key
    willing to expose because:
     - it is a free subscription
     - only allows GET requests
     - it's for a toy app

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,4 +1,5 @@
 export default async function fetchData(location = "Boston") {
+  const key = "&appid=0ef688ca35703421279c3bde4d9ca4e6";
   const query = "q=" + location;
   const units = "&units=imperial"
   const baseUrl = "https://api.openweathermap.org/data/2.5/weather?";
