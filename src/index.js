d8ec9cb3cef03bc74543bdfffae11957fe88facd
Nick <nsardonini@gmail.com>
2021-01-20 21:17:31 -0500

    wrap logic in function to prevent global namespace pollution

--- src/index.js
+++ src/index.js
@@ -3,52 +3,54 @@ import fetchData from "./fetchData";
 import processData from "./processData";
 import createElements from "./createElements";
 
-function fetchAndCreate(location = "Boston") {
-  fetchData(location)
-    .then((data) => processData(data))
-    .then((data) => createElements(data))
-    .catch((err) => handleError(err))
-}
-
-function handleError(err) {
-  console.log(err)
-  const body = document.querySelector("body");
-  const msg = document.createElement("h2");
-
-  msg.innerText = "I couldn't find that place, sorry!";
-
-  msg.classList.add("app-wrapper");
-  msg.classList.add("error-text");
-
-  body.appendChild(msg);
-}
-
-function clearCurrent() {
-  const wrapper = document.querySelectorAll(".app-wrapper");
-  const img = document.querySelectorAll(".bg-img");
-
-  if ([...img].length > 0) {
-    [...img].forEach((image) => image.remove());
+(function main() {
+  function fetchAndCreate(location = "Boston") {
+    fetchData(location)
+      .then((data) => processData(data))
+      .then((data) => createElements(data))
+      .catch((err) => handleError(err))
   }
 
-  if ([...wrapper].length > 0) {
-    while (wrapper.firstChild) {
-      wrapper.firstChild.remove();
+  function handleError(err) {
+    console.log(err)
+    const body = document.querySelector("body");
+    const msg = document.createElement("h2");
+
+    msg.innerText = "I couldn't find that place, sorry!";
+
+    msg.classList.add("app-wrapper");
+    msg.classList.add("error-text");
+
+    body.appendChild(msg);
+  }
+
+  function clearCurrent() {
+    const wrapper = document.querySelectorAll(".app-wrapper");
+    const img = document.querySelectorAll(".bg-img");
+
+    if ([...img].length > 0) {
+      [...img].forEach((image) => image.remove());
     }
 
-    [...wrapper].forEach((wrap) => wrap.remove());
+    if ([...wrapper].length > 0) {
+      while (wrapper.firstChild) {
+        wrapper.firstChild.remove();
+      }
+
+      [...wrapper].forEach((wrap) => wrap.remove());
+    }
   }
-}
 
-(function app() {
-  const btn = document.querySelector(".search-submit");
-  const q = document.querySelector(".search");
+  (function app() {
+    const btn = document.querySelector(".search-submit");
+    const q = document.querySelector(".search");
 
-  btn.addEventListener("click", () => {
-    const qArg = q.value || "Boston";
+    btn.addEventListener("click", () => {
+      const qArg = q.value || "Boston";
 
-    clearCurrent();
+      clearCurrent();
 
-    fetchAndCreate(qArg);
-  });
+      fetchAndCreate(qArg);
+    });
+  })();
 })();
