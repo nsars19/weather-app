9b8f28d29c8ed81908414135e0fb3fb29f1d7ef8
Nick <nsardonini@gmail.com>
2021-01-20 20:14:05 -0500

    remove argument from fetchData call

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import fetchData from "./fetchData";
 import processData from "./processData";
 import createElements from "./createElements";
 
-fetchData("iceland")
+fetchData()
   .catch(err => console.log(err))
   .then((data) => processData(data))
   .then((data) => createElements(data));
